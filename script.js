5fdc510f1b976e0c0d587686e8deaad576b92061
Oleg Kit <oleg2448@gmail.com>
2019-07-07 21:16:31 +0300

    New version

--- script.js
+++ script.js
@@ -38,36 +38,31 @@ $("#insert").click(function() {
 });
 
 
-$("#text").on("mouseup", function(e) {
-    if(window.getSelection)
-       txt = window.getSelection().toString();
-    else if(document.getSelection)
-       txt = document.getSelection();
-    else if(document.selection)
-       txt = document.selection.createRange().text;
-    $('#URL').css({ display: 'none', width: 'auto'}).text('');
-    if (txt.length != 0){
-        $('#URL').text(txt);
-        txt_length = $('#URL').width();
-        $('#URL').css({ position: 'absolute', left: e.pageX - txt_length, top: e.pageY - 75});
-        $('#URL').html('<p><label for="URL_adress">URL</label><input type="text" id="URL_adress"><button type="button" id="attach_picture">Attach</button> </p>');
-        $('#URL').show().css({ width: 280});
-        $("#attach_picture").on("click", function(){
-            text_string = $("#text").html()
-            re = new RegExp(txt, 'g');
-            new_text_string = text_string.replace(re, "<span  class='picture' adress=" + $("#URL_adress").val() + "><strong>" + txt + "</strong></span>")
-            document.getElementById('text').innerHTML = new_text_string;
-            $('#URL').css({ display: 'none', width: 'auto'}).text('');
-        });
+$("#attach").on("click", function() {
+    if ($('#URL').css("display") !== 'none'){ 
+        $('#URL').css({ display: 'none', width: 'auto'});
+    } 
+    else{
+        $('#URL').css({ display: 'block', width: '200px'});
     }
+    $("#attach_picture").on("click", function(){
+        if ($("#word").val() &&  $("#URL_adress").val()){
+        txt = $("#word").val();
+        text_string = $("#text").html()
+        re = new RegExp(txt, 'g');
+        new_text_string = text_string.replace(re, "<span  class='picture' adress=" + $("#URL_adress").val() + "><strong>" + txt + "</strong></span>")
+        document.getElementById('text').innerHTML = new_text_string;
+        };
+    });
 });
 
 $("#text").on("mouseenter", 
   ".picture", function(e) {
-    $('#picture').html('<img src=' + $(this).attr("adress") + '>');
     image_width = $('#picture').width();
     image_height = $('#picture').height();
-    $('#picture').css({position: 'absolute', left: e.pageX - image_width/2, top: e.pageY - image_height - 10}).show();
+    $('#picture').css({position: 'absolute', left: e.pageX - image_width/2, top: e.pageY - image_height - 10});
+    $('#picture').html('<img src=' + $(this).attr("adress") + '>');
+    $('#picture').show();
 });
 
 $("#text").on("mouseleave", 
