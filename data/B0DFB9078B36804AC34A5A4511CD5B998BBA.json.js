357e4cd8cfe8998afa73544de7f1bfef94e59a28
OlegKitGit <oleg2448@gmail.com>
2022-02-01 13:45:15 +0200

    123
    123

---
+++ data/B0DFB9078B36804AC34A5A4511CD5B998BBA.json.js
@@ -0,0 +1,52 @@
+GS.dataCache['B0DFB9078B36804AC34A5A4511CD5B998BBA'] = {
+    "uid": "B0DFB9078B36804AC34A5A4511CD5B998BBA",
+    "isLoaded": true,
+    "lastModificationTime": null,
+    "items": {
+        "name": "2",
+        "type": "data_record",
+        "category": "cgGallery",
+        "id": "B0DFB9078B36804AC34A5A4511CD5B998BBA",
+        "isFolder": false,
+        "parentId": "ROOT",
+        "data": {
+            "name": "2",
+            "listed": true,
+            "graphic": {
+                "name": "1",
+                "hue": 0,
+                "opacity": 255,
+                "blending": 0,
+                "tone": {
+                    "red": 0,
+                    "green": 0,
+                    "blue": 0
+                }
+            },
+            "thumb": {
+                "name": "1",
+                "hue": 0,
+                "opacity": 255,
+                "blending": 0,
+                "tone": {
+                    "red": 0,
+                    "green": 0,
+                    "blue": 0
+                }
+            },
+            "index": "B0DFB9078B36804AC34A5A4511CD5B998BBA",
+            "relationData": {
+                "chapter": {
+                    "uid": "390E79F68F0C6740EA6871B25EB387239F0B",
+                    "name": "New Chapter"
+                }
+            }
+        },
+        "order": 1
+    },
+    "summary": [
+        "name",
+        "type",
+        "order"
+    ]
+}
---
+++ data/RESOURCES.json.js
@@ -0,0 +1,107 @@
+GS.dataCache['RESOURCES'] = {
+    "uid": "RESOURCES",
+    "items": {
+        "type": "resource_table",
+        "resources": {
+            "Graphics/Backgrounds/4": ".jpg",
+            "Graphics/Backgrounds/white_logo_color_background": ".jpg",
+            "Graphics/Pictures/m-arrow": ".png",
+            "Graphics/Pictures/m-arrow-disabled": ".png",
+            "Graphics/Pictures/m-diamond-large-off": ".png",
+            "Graphics/Pictures/m-diamond-large-on": ".png",
+            "Graphics/Pictures/m-diamond-small-off": ".png",
+            "Graphics/Pictures/m-diamond-small-on": ".png",
+            "Graphics/Pictures/msg-diamond-small-off": ".png",
+            "Graphics/Pictures/msg-diamond-small-on": ".png",
+            "Graphics/Pictures/bg-generic": ".png",
+            "Graphics/Pictures/diamond-large-off": ".png",
+            "Graphics/Pictures/diamond-large-on": ".png",
+            "Graphics/Pictures/diamond-small-off": ".png",
+            "Graphics/Pictures/diamond-small-on": ".png",
+            "Graphics/Pictures/dropshadow": ".png",
+            "Graphics/Pictures/entrybox": ".png",
+            "Graphics/Pictures/locked": ".png",
+            "Graphics/Pictures/scrollbar": ".png",
+            "Graphics/Pictures/selection": ".png",
+            "Graphics/Pictures/selection-inactive": ".png",
+            "Graphics/Pictures/skin-frame": ".png",
+            "Graphics/Pictures/skin-stretch": ".png",
+            "Graphics/Pictures/skin-tile": ".png",
+            "Graphics/Pictures/message_caret": ".png",
+            "Graphics/Pictures/thumbnailshadow": ".png",
+            "Graphics/Pictures/1": ".jpg",
+            "Graphics/Pictures/2": ".jpg",
+            "Graphics/Pictures/3": ".jpg",
+            "Graphics/Pictures/4": ".jpg",
+            "Graphics/Pictures/5": ".jpg",
+            "Graphics/Pictures/6": ".jpg",
+            "Graphics/Pictures/7": ".jpg",
+            "Graphics/Pictures/8": ".jpg",
+            "Graphics/Pictures/9": ".jpg",
+            "Graphics/Pictures/10": ".jpg",
+            "Graphics/Pictures/11": ".jpg",
+            "Graphics/Pictures/12": ".jpg",
+            "Graphics/Pictures/13": ".jpg",
+            "Graphics/Pictures/14": ".jpg",
+            "Graphics/Pictures/15": ".jpg",
+            "Graphics/Pictures/16": ".jpg",
+            "Graphics/Pictures/17": ".jpg",
+            "Graphics/Pictures/18": ".jpg",
+            "Graphics/Pictures/19": ".jpg",
+            "Graphics/Pictures/20": ".jpg",
+            "Graphics/Pictures/21": ".jpg",
+            "Graphics/Pictures/web-start-screen": ".png",
+            "Graphics/Icons/Germany": ".png",
+            "Graphics/Icons/Japan": ".png",
+            "Graphics/Icons/United-Kingdom": ".png",
+            "Audio/Sounds/choice_cancel_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/choice_confirm_01": [
+                ".ogg"
+            ],
+            "Audio/Sounds/choice_confirm_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/choice_confirm_03": [
+                ".ogg"
+            ],
+            "Audio/Sounds/cursor": [
+                ".ogg"
+            ],
+            "Audio/Sounds/exit_01": [
+                ".ogg"
+            ],
+            "Audio/Sounds/exit_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/invalid_buzz_01": [
+                ".ogg"
+            ],
+            "Audio/Sounds/invalid_buzz_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/invalid_buzz_03": [
+                ".ogg"
+            ],
+            "Audio/Sounds/load_01": [
+                ".ogg"
+            ],
+            "Audio/Sounds/load_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/save_01": [
+                ".ogg"
+            ],
+            "Audio/Sounds/save_02": [
+                ".ogg"
+            ],
+            "Audio/Sounds/choice_cancel_01": [
+                ".ogg"
+            ],
+            "Movies/film": [
+                ".webm"
+            ]
+        }
+    }
+}
